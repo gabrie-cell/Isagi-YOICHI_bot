65146673407648fcb6a11af00c6bdf27d6633266
⏤͟͟͞͞☆  𝑫𝒆𝒗-𝑩𝒍𝒂𝒄𝒌𝑫𝒂𝒓𝒌  ❀ <fedexyz13@gmail.com>
2025-10-09 23:06:12 -0300

    Update owner-update.js

--- plugins/owner-update.js
+++ plugins/owner-update.js
@@ -1,24 +1,62 @@
 // Código de Dev-fedexyz13
 
-import { exec} from 'child_process';
 
-const handler = async (m, { conn, command}) => {
-  const isOwner = global.owner.includes(m.sender);
-  if (!isOwner) return m.reply('🚫 *_Este comando solo puede ser usado por el owner._*');
+import { execSync} from 'child_process';
 
-  m.reply('⚙ *_Ejecutando actualización del bot..._*');
+const handler = async (m, { conn, text, isROwner}) => {
+  if (!isROwner) return;
 
-  exec('git pull', (err, stdout, stderr) => {
-    if (err) {
-      conn.reply(m.chat, `❌ Error al actualizar:\n${stderr}`, m);
-} else {
-      conn.reply(m.chat, `✅ Bot actualizado correctamente:\n${stdout}`, m);
+  await m.react('🕒'); 
+
+  try {
+    const stdout = execSync('git pull' + (m.fromMe && text? ' ' + text: ''));
+    let output = stdout.toString();
+
+    if (output.includes('⚙ Ya está cargada la actualización.')) {
+      output = '✅ Los datos ya están actualizados a la última versión.';
+} else if (output.includes('👻 Actualizando.')) {
+      output = '🔄 Procesando actualización...\n\n' + output;
 }
-});
+
+    await m.react('✔️'); 
+    conn.reply(m.chat, output, m);
+
+} catch {
+    try {
+      const status = execSync('git status --porcelain');
+      if (status.length> 0) {
+        const conflictedFiles = status
+.toString()
+.split('\n')
+.filter(line => line.trim()!== '')
+.map(line => {
+            const ignoredPaths = [
+              '.npm/', '.cache/', 'tmp/',
+              'database.json', 'sessions/Principal/',
+              'npm-debug.log'
+            ];
+            return ignoredPaths.some(path => line.includes(path))? null: `*→ ${line.slice(3)}*`;
+})
+.filter(Boolean);
+
+        if (conflictedFiles.length> 0) {
+          const errorMsg = `⚠️ *Actualización fallida:*\n\n> Se detectaron cambios locales que entran en conflicto con el repositorio remoto.\n\n${conflictedFiles.join('\n')}`;
+          await conn.reply(m.chat, errorMsg, m);
+          await m.react('✖️');
+}
+}
+} catch (error) {
+      console.error(error);
+      let fallbackMsg = '⚠️ Se produjo un error inesperado.';
+      if (error.message) fallbackMsg += `\n🧩 Detalles: ${error.message}`;
+      await conn.reply(m.chat, fallbackMsg, m);
+}
+}
 };
 
-handler.command = ['update', 'fix', 'up'];
+handler.help = ['update'];
+handler.tags = ['owner'];
+handler.command = ['update', 'fix', 'actualizar'];
 handler.owner = true;
-handler.group = false;
 
 export default handler;
