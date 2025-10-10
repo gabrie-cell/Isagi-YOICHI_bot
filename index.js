5f3ea111fec2b8d33dd475752306e1f5c00105d6
✨ ˚₊ ʜ ᴀ s ʜ ɪ ʀ ᴀ ᴍ ᴀ・୧ <brayanphonkponk@gmail.com>
2025-10-09 19:50:32 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
 import './settings.js'
-import './plugins/main-allfake.js'
 import cfonts from 'cfonts'
 import { createRequire } from 'module'
 import { fileURLToPath, pathToFileURL } from 'url'
