84af439ea9f102e82f540e67dba88c2cdfb71f26
⏤͟͟͞͞☆  𝑫𝒆𝒗-𝑩𝒍𝒂𝒄𝒌𝑫𝒂𝒓𝒌  ❀ <fedexyz13@gmail.com>
2025-10-11 20:34:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '1'
-import './settings.js'
+import './config.js'
 import cfonts from 'cfonts'
 import { createRequire } from 'module'
 import { fileURLToPath, pathToFileURL } from 'url'
